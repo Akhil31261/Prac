a7bea71772463e6073981364104d6f5c3285e1b1
Akhil31261 <77038112+Akhil31261@users.noreply.github.com>
2021-01-20 12:22:44 +0530

    Update bi.py

--- bi.py
+++ bi.py
@@ -1,6 +1,6 @@
 import random
 print("Hello There")
-a = random.randint(0,9)
-b = random.randint(0,9)
+a = random.randint(0,100)
+b = random.randint(0,100)
 print("Sum of Random two numbers is",a+b)
-print("Your luck number is",random.randint(0,9))
+print("Your luck number is",random.randint(0,100))
